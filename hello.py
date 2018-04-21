a380ee3804a55255c42ca14e57eed1d7f2841f7b
lyric777 <37208917+lyric777@users.noreply.github.com>
2018-04-21 15:38:10 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -23,8 +23,8 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
-        return redirect(url_for('base_kid'))
-    return render_template('base_kid.html', form=form, name=session.get('name'))
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
